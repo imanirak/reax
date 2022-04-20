b47ae3f97240ba10a543a4a635c4b8e053ba174e
imanirak <karinadgarces@gmail.com>
2022-04-20 16:35:45 -0400

    env_test

--- server/server.js
+++ server/server.js
@@ -1,43 +1,45 @@
-
 const path = require("path")
-require("dotenv").config({path: "../.env"})
+const dotenv = require('dotenv')
+require("dotenv").config({path:"../.env"})
 
-// **** External modules
+
+/* ==== External Modules ==== */
 const express = require("express");
 const cors = require("cors")
+/* ==== Internal Modules ==== */
 
-//***** Internal Modules
-
-
-
-//**** Instanced Modules
+/* ==== Instanced Modules  ==== */
 const app = express();
 const routes = require("./routes");
+/* ==== Configuration ==== */
+const config = require("@reax/config");
 
-//****Configs
-const config = require('@reax/config');
+/* ==== Middleware ==== */
 
-
-//****middleware
 app.use(express.static(path.join("build")))
 // //this helps us read the body, req.body also works with Postman
 app.use(express.urlencoded({extended: true}))
-
+// //parse some json :)))
 app.use(express.json())
 app.use(cors())
 
-
 /* ====  Routes & Controllers  ==== */
-app.use("/api", routes)
+// app.use("/api", routes)
 
 app.all("/api/*", (req, res, next) => {
  	res.send("HOLD UP THESE ARE NOT THE APIS YOU ARE LOOKING FOR")
  })
 
+//This targets all routes that aren't specified by our specific server routes that are not "/api"
+//ANY REQUESTS not covered by our routes will get piped into this middleware! This literally hands over control to React
+app.use((req, res, next) => {
+//res.sendFile(path.join(__dirname, "build", "index.html"))
+})
 
 
-
-//****server listener
+/* ====  Server Listener  ==== */
 app.listen(config.PORT, () => {
-    console.log(`reax is live! *** live at http://localhost:${config.PORT}`)
+	console.log(
+		`reax is live at http://localhost:${config.PORT}.`
+	);
 })
